d8880d3c84b1b7533deff409a9c58b692981cd8e
Manuel Chiquero <manuchiquero@hotmail.com>
2024-11-13 10:10:30 +0100

    Ejercicio 72 terminado

--- src/Ejercicio72.java
+++ src/Ejercicio72.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Ejercicio72 {
     public static void main(String[] args) {
-        Scanner ejercicio72 = new Scanner(System.in);
         for (int i = 0; i <100 ; i++) {
             if (i % 5 ==0 && i% 3 !=0){
                 System.out.println(i);
